3538a76d4dfbf9e39322cef15b06e797d2583c1d
Tiago Do Couto <cs.couto@outlook.com>
2025-12-09 18:02:20 +1300

    removed comments

--- allcountries/allcountriesApp.swift
+++ allcountries/allcountriesApp.swift
@@ -11,27 +11,11 @@ import SwiftData
 @main
 struct allcountriesApp: App {
     @StateObject private var navVM = NavigationViewModel()
-    
-//Setup for Swift Data
-    
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            Item.self,
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
 
     var body: some Scene {
         WindowGroup {
             CountryListView()
                 .environmentObject(navVM)
         }
-//        .modelContainer(sharedModelContainer)
     }
 }
